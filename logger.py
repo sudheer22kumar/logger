039fbc574781da0210d92c4f8322ea48692711fb
Sudheer Kumar P. <sudheer22kumar@users.noreply.github.com>
2025-09-25 22:07:00 +0530

    Enhanced logging.

--- logger.py
+++ logger.py
@@ -1,96 +1,94 @@
+import json
 import logging as _logging
+import re
+from pprint import pformat
+
+
+class LoggerException(Exception):
+    pass
+
+
+class PrettyEmbeddedJSONFormatter(_logging.Formatter):
+    json_regex = re.compile(r'(\{.*}|\[.*])')  # crude but works for simple dict/list
+
+    def format(self, record):
+        msg = record.getMessage()
+
+        # Find all JSON/dict-like substrings
+        def pretty_sub(match):
+            text = match.group(0)
+            try:
+                parsed = json.loads(text)
+                return json.dumps(parsed, indent=4)
+            except json.JSONDecodeError:
+                # If it's Python-style dict, use eval cautiously
+                try:
+                    parsed = eval(text, {"__builtins__": None}, {})
+                    if isinstance(parsed, (dict, list)):
+                        return pformat(parsed, indent=4)
+                except LoggerException:
+                    pass
+            return text
+
+        pretty_msg = self.json_regex.sub(pretty_sub, msg)
+        record.msg = pretty_msg
+        record.args = ()
+        return super().format(record)
 
 
 class Log:
-    def __init__(self, name=__name__):
+    def __init__(self, name=__name__, pretty_format=False):
         self.name = name
+        self.pretty_format = pretty_format
         if "." in name:
             self.logger = _logging.getLogger(name=self.name.split(".")[0]).getChild(".".join(self.name.split(".")[1:]))
         else:
             self.logger = _logging.getLogger(name=self.name)
 
     def set_handler(
-        self,
-        level="warn",
-        formatter="%(asctime)s - %(name)s - %(levelname)s \t %(message)s",
+            self,
+            level="warn",
+            formatter="%(asctime)s - %(name)s - %(levelname)s \t %(message)s",
     ):
         """<type_>: "Critical"/ "Error" / "Warning"/ "Info"/ "Debug" """
 
+        if self.pretty_format:
+            _formatter = PrettyEmbeddedJSONFormatter(formatter)
+        else:
+            _formatter = _logging.Formatter(formatter)
+        _handler = _logging.StreamHandler()
+
         if level.lower().startswith("c"):
-            _handler = _logging.StreamHandler()
             _handler.setLevel(_logging.CRITICAL)
-            _handler.setFormatter(_logging.Formatter(formatter))
-            if not self.logger.hasHandlers():
-                self.logger.addHandler(_handler)
-                self.logger.setLevel("CRITICAL")
-                self.logger.propagate = False
-            else:
-                handler_list = [str(x).strip("<>").split("stderr")[1].strip(" ()") for x in self.logger.handlers]
-                if "CRITICAL" not in handler_list:
-                    self.logger.addHandler(_handler)
-                    self.logger.setLevel("CRITICAL")
-                    self.logger.propagate = False
+            level_to_set = "CRITICAL"
 
         elif level.lower().startswith("e"):
-            _handler = _logging.StreamHandler()
             _handler.setLevel(_logging.ERROR)
-            _handler.setFormatter(_logging.Formatter(formatter))
-            if not self.logger.hasHandlers():
-                self.logger.addHandler(_handler)
-                self.logger.setLevel("ERROR")
-                self.logger.propagate = False
-            else:
-                handler_list = [str(x).strip("<>").split("stderr")[1].strip(" ()") for x in self.logger.handlers]
-                if "ERROR" not in handler_list:
-                    self.logger.addHandler(_handler)
-                    self.logger.setLevel("ERROR")
-                    self.logger.propagate = False
+            level_to_set = "ERROR"
 
         elif level.lower().startswith("w"):
-            _handler = _logging.StreamHandler()
             _handler.setLevel(_logging.WARNING)
-            _handler.setFormatter(_logging.Formatter(formatter))
-            if not self.logger.hasHandlers():
-                self.logger.addHandler(_handler)
-                self.logger.setLevel("WARNING")
-                self.logger.propagate = False
-            else:
-                handler_list = [str(x).strip("<>").split("stderr")[1].strip(" ()") for x in self.logger.handlers]
-                if "WARNING" not in handler_list:
-                    self.logger.addHandler(_handler)
-                    self.logger.setLevel("WARNING")
-                    self.logger.propagate = False
-
-        elif level.lower().startswith("i"):
-            _handler = _logging.StreamHandler()
-            _handler.setLevel(_logging.INFO)
-            _handler.setFormatter(_logging.Formatter(formatter))
-            if not self.logger.hasHandlers():
-                self.logger.addHandler(_handler)
-                self.logger.setLevel("INFO")
-                self.logger.propagate = False
-
-            else:
-                handler_list = [str(x).strip("<>").split("stderr")[1].strip(" ()") for x in self.logger.handlers]
-                if "INFO" not in handler_list:
-                    self.logger.addHandler(_handler)
-                    self.logger.setLevel("INFO")
-                    self.logger.propagate = False
+            level_to_set = "WARNING"
 
         elif level.lower().startswith("d"):
-            _handler = _logging.StreamHandler()
             _handler.setLevel(_logging.DEBUG)
-            _handler.setFormatter(_logging.Formatter(formatter))
-            if not self.logger.hasHandlers():
+            level_to_set = "DEBUG"
+
+        else:
+            _handler.setLevel(_logging.INFO)
+            level_to_set = "INFO"
+
+        _handler.setFormatter(_formatter)
+        if not self.logger.hasHandlers():
+            self.logger.addHandler(_handler)
+            self.logger.setLevel(level_to_set)
+            self.logger.propagate = False
+        else:
+            handler_list = [str(x).strip("<>").split("stderr")[1].strip(" ()") for x in self.logger.handlers]
+            if level_to_set not in handler_list:
                 self.logger.addHandler(_handler)
-                self.logger.setLevel("DEBUG")
+                self.logger.setLevel(level_to_set)
                 self.logger.propagate = False
-            else:
-                handler_list = [str(x).strip("<>").split("stderr")[1].strip(" ()") for x in self.logger.handlers]
-                if "DEBUG" not in handler_list:
-                    self.logger.addHandler(_handler)
-                    self.logger.setLevel("DEBUG")
-                    self.logger.propagate = False
 
 
 def loggers():
